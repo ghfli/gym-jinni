5788aff80418f9de3fc00cb84da6b8cb3b92f225
GJ <newstart.infotech@gmail.com>
2022-07-03 12:37:34 -0700

    squash

--- services/main.go
+++ services/main.go
@@ -4,23 +4,20 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	userv1 "github.com/ghfli/gym-jinni/services/gen/proto/go/user/v1"
-	"github.com/golang/glog"
+	usermgt "github.com/ghfli/gym-jinni/services/usermgt"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"log"
-	"math/rand"
 	"net"
 	"net/http"
 )
 
 func main() {
 	flag.Parse()
-	defer glog.Flush()
 
 	if err := run(); err != nil {
-		glog.Fatal(err)
+		log.Fatal(err)
 	}
 }
 
@@ -42,7 +39,7 @@ func runGRPCServer() error {
 	}
 
 	server := grpc.NewServer()
-	userv1.RegisterUserMgtServiceServer(server, &userMgtServiceServer{})
+	usermgt.RegisterUserMgtServiceServer(server)
 	log.Println("gRPC server listening on", *grpcServerEndpoint)
 	if err := server.Serve(listener); err != nil {
 		return fmt.Errorf("failed to serve gRPC server: %w", err)
@@ -58,7 +55,7 @@ func runGatewayServer() error {
 
 	mux := runtime.NewServeMux()
 	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
-	err := userv1.RegisterUserMgtServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+	err := usermgt.RegisterUserMgtServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
 
 	if err != nil {
 		return err
@@ -66,17 +63,4 @@ func runGatewayServer() error {
 
 	log.Println("gRPC gateway server listening on :8081")
 	return http.ListenAndServe(":8081", mux)
-}
-
-type userMgtServiceServer struct {
-	userv1.UnimplementedUserMgtServiceServer
-}
-
-func (s *userMgtServiceServer) CreateUser(ctx context.Context, req *userv1.CreateUserRequest) (*userv1.CreateUserResponse, error) {
-	user := req.GetUser()
-	log.Println("Got a request to create user with", user)
-	user.Id = rand.Uint32()
-
-	log.Println("Responding with", user)
-	return &userv1.CreateUserResponse{User: user}, nil
 }
