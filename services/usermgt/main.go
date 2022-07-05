d5f1ab6da6aa9afcac6d7fee3651ffda61f8e433
GJ <newstart.infotech@gmail.com>
2022-07-04 19:32:37 -0700

    conform with go module convention

--- services/usermgt/main.go
+++ services/usermgt/main.go
@@ -1,33 +1,22 @@
-package usermgtv1
+package usermgt
 
 import (
 	"context"
-	usermgtv1 "github.com/ghfli/gym-jinni/services/gen/go/usermgt/v1"
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-	grpc "google.golang.org/grpc"
+	. "github.com/ghfli/gym-jinni/services/gen/go/usermgt/v1"
 	"log"
 	"math/rand"
 )
 
-type userMgtServiceServer struct {
-	usermgtv1.UnimplementedUserMgtServiceServer
+type ImUserMgtServiceServer struct {
+	UnimplementedUserMgtServiceServer
 }
 
-func (s *userMgtServiceServer) CreateUser(ctx context.Context,
-	req *usermgtv1.CreateUserRequest) (*usermgtv1.CreateUserResponse, error) {
+func (s *ImUserMgtServiceServer) CreateUser(ctx context.Context,
+	req *CreateUserRequest) (*CreateUserResponse, error) {
 	user := req.GetUser()
 	log.Println("Got a request to create user with", user)
 	user.Id = rand.Uint32()
 
 	log.Println("Responding with", user)
-	return &usermgtv1.CreateUserResponse{User: user}, nil
-}
-
-func RegisterUserMgtServiceServer(s grpc.ServiceRegistrar) {
-	usermgtv1.RegisterUserMgtServiceServer(s, &userMgtServiceServer{})
-}
-
-func RegisterUserMgtServiceHandlerFromEndpoint(ctx context.Context,
-	mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error {
-	return usermgtv1.RegisterUserMgtServiceHandlerFromEndpoint(ctx, mux, endpoint, opts)
+	return &CreateUserResponse{User: user}, nil
 }
