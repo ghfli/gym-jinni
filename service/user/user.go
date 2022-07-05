7b0d5878364fe07d7234b18028114d7304d63833
GJ <newstart.infotech@gmail.com>
2022-07-05 16:51:52 -0700

    rename/reorganize files

---
+++ service/user/user.go
@@ -0,0 +1,22 @@
+package user
+
+import (
+	"context"
+	. "github.com/ghfli/gym-jinni/service/gen/go/user/v1"
+	"log"
+	"math/rand"
+)
+
+type ImUserServiceServer struct {
+	UnimplementedUserServiceServer
+}
+
+func (s *ImUserServiceServer) CreateUser(ctx context.Context,
+	req *CreateUserRequest) (*CreateUserResponse, error) {
+	user := req.GetUser()
+	log.Println("Got a request to create user with", user)
+	user.Id = rand.Uint32()
+
+	log.Println("Responding with", user)
+	return &CreateUserResponse{User: user}, nil
+}
